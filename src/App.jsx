6f56b08d51398a0ec339e23571504de182bff786
Masud Rana <masud.cphd@gmail.com>
2024-03-02 16:39:15 +0600

    workway

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from './Components/Services/Services'
 import Header from './Components/Headers/Header'
 import WorkTogether from './Components/WorkTogether/WorkTogether'
 import Connect from './Components/Connect/Connect'
+import WorkWay from './Components/WorkWay/WorkWay'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Services></Services>
         <WorkTogether></WorkTogether>
         <Connect></Connect>
+        <WorkWay></WorkWay>
       </div>
     </>
   )
---
+++ src/Components/WorkWay/WorkWay.jsx
@@ -0,0 +1,20 @@
+import img from '../../../public/images/section.png'
+import { MdArrowRightAlt } from 'react-icons/md'
+const WorkWay = () => {
+    return (
+        <section className="max-w-container mx-auto pb-20 px-48">
+            <div className='relative'>
+                <img src={img} alt="" />
+            </div>
+            <div className='absolute top-[325%] left-[55%] text-center '>
+                <h2 className='text-[18px] font-bold mb-2'>The Ways We Work</h2>
+                <p>How has our relationship with work changed?</p>
+                <div className="btn flex justify-center mt-2">
+                    <button className="border border-[#4262FF] rounded-[100px] px-3 py-3 bg-[#4262FF] transition hover:bg-white hover:text-black text-white flex items-center ">View the report<i className="ml-1"><MdArrowRightAlt /></i></button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default WorkWay
